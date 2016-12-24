69873532e9c85b24a28211c09aadb8be4bc62112
realityone <realityone@me.com>
2016-12-25 00:33:42 +0800

    fix pypi package

--- setup.py
+++ setup.py
@@ -6,15 +6,17 @@ from setuptools import setup
 ROOT_DIR = os.path.dirname(__file__)
 SOURCE_DIR = os.path.join(ROOT_DIR)
 
-with open(os.path.join(SOURCE_DIR, 'requirements.txt'), 'r') as f:
-    requirements = f.read().splitlines()
+install_requires = [
+    'enum34>=1.1.6',
+    'cryptography>=1.5.2',
+]
 
 setup(
     name="libtrust-py",
-    version='0.1',
+    version='0.1.1',
     description="Integrate docker/libtrust with python.",
     url='https://github.com/realityone/libtrust-py',
     packages=['libtrust'],
-    install_requires=requirements,
+    install_requires=install_requires,
     zip_safe=False,
 )
